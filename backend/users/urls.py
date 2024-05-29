617d12c05b7cb41789aabcae3bd6ff5482caf6c5
awsl1414 <3030994569@qq.com>
2024-05-29 16:31:48 +0800

    使用pdm管理依赖

--- backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import UserRegistrationView, AvatarUploadView, CustomObtainAuthToken
-
-urlpatterns = [
-    path("register/", UserRegistrationView.as_view(), name="register"),
-    path("avatar/", AvatarUploadView.as_view(), name="avatar"),
-    path("login/", CustomObtainAuthToken.as_view(), name="login"),
-]
